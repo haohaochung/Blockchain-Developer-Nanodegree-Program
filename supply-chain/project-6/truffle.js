0292fa87bf631182bbf703455bcf855bf9665fd5
colin <colin19940702@gmail.com>
2022-01-31 03:20:02 +0800

    create supply chian project

---
+++ supply-chain/project-6/truffle.js
@@ -0,0 +1,27 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = "a701e782af3d4c1b8352a428e9eb90b4";
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    }, 
+    rinkeby: {
+      networkCheckTimeout: 10000, 
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+        network_id: 4,       // rinkeby's id
+        gas: 45000000,        // rinkeby has a lower block limit than mainnet
+        gasPrice: 10000000000
+    }
+  },
+  compilers: {
+    solc: {
+      version: "^0.4.23"
+    }
+  }
+};
